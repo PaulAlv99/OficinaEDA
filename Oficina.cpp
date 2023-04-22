3b1c3ae261acc48f56126c0cbfe7fac7a6136703
renatopessego <renatoDESKTOP>
2023-04-22 14:42:41 +0100

    Ta la. Faz o crlh do push da gestao leo

--- Oficina.cpp
+++ Oficina.cpp
@@ -45,6 +45,7 @@ void seguinte(Oficina& Of, LinhasFicheiro& marcas,LinhasFicheiro& modelos)
 //sugiro usar a funcao bool  GerarProbalidades(double probalidade);
 void reparacao(EstacaoTrabalho & ID1) {
 	int chance;
+	
 	for (int i = 0; i < ID1.capacidade; i++) {
 		if ((ID1.carros_a_ser_reparados[i].dias_em_reparacao < ID1.carros_a_ser_reparados[i].tempo_reparacao_max)&&(ID1.carros_a_ser_reparados[i].ID != 0)) {
 			chance = rand() % 100 + 1;
@@ -54,7 +55,7 @@ void reparacao(EstacaoTrabalho & ID1) {
 				//para usar aqui na oficina acho que basta esta declarado no header a funcao e os parametros
 				//e passar sempre como argumentos o endereço (&)
 				
-				//Adiciona(ID1.Carrosreparados, ID1.num_carros_reparados, ID1.carros_a_ser_reparados[i]);
+				ID1.Carrosreparados[ID1.num_carros_reparados] = ID1.carros_a_ser_reparados[i];
 				ID1.num_carros_reparados = ID1.num_carros_reparados + 1;
 				ID1.num_carros_a_ser_reparados = ID1.num_carros_a_ser_reparados - 1;
 				ID1.carros_a_ser_reparados[i].ID = 0;
@@ -66,6 +67,7 @@ void reparacao(EstacaoTrabalho & ID1) {
 
 		else if ((ID1.carros_a_ser_reparados[i].dias_em_reparacao >= ID1.carros_a_ser_reparados[i].tempo_reparacao_max) && (ID1.carros_a_ser_reparados[i].ID != 0)) {
 				//Adiciona(ID1.Carrosreparados, ID1.num_carros_reparados, ID1.carros_a_ser_reparados[i]);
+				ID1.Carrosreparados[ID1.num_carros_reparados] = ID1.carros_a_ser_reparados[i];
 				ID1.num_carros_reparados = ID1.num_carros_reparados + 1;
 				ID1.num_carros_a_ser_reparados = ID1.num_carros_a_ser_reparados - 1;
 				ID1.carros_a_ser_reparados[i].ID = 0;
