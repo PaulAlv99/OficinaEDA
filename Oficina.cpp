8a98281d19372b2816b6bd4ac8cde45295ad6b8c
Paulo <2120722@student.uma.pt>
2023-04-23 17:55:09 +0100

    .

--- Oficina.cpp
+++ Oficina.cpp
@@ -33,8 +33,9 @@ void seguinte(Oficina& Of, LinhasFicheiro& marcas,LinhasFicheiro& modelos)
 {
 	Of.ciclos++;
 	CriarCarrosNaFila(Of, marcas, modelos, 10);
-	colocarprioritario(Of);
 	ColocarCarrosET(Of, 8);
+	colocarprioritario(Of);
+	
 	for (int i = 0; i < Of.numero_ets; i++) {
 		reparacao(Of.ets[i]);
 
