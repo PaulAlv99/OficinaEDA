69d234b06c5d02e8f3fe1cdae7eaf6532c1eeae4
Paulo <2120722@student.uma.pt>
2023-04-28 19:46:49 +0100

    ligada simples ou duplamente ligada?

---
+++ ListaLigada.cpp
@@ -0,0 +1,65 @@
+#include "ListLigada.h"
+
+//numa ET
+int num_carros_a_ser_reparados(nodoDuplo* head) {
+	nodoDuplo* current = head;
+	int count = 0;
+	while (current->seguinte != NULL) {
+		count++;
+		current = current->seguinte;
+	}
+	return count;
+}
+
+void insereFim(nodoDuplo*& head, Carro& carro_a_receber) {
+	nodoDuplo* iterator = head;
+	while (iterator->seguinte != NULL) {
+	iterator = iterator->seguinte;
+	}
+	nodoDuplo* newNode = new nodoDuplo;
+	newNode->seguinte = NULL;
+	newNode->carros_a_ser_reparados = carro_a_receber;
+
+	iterator->seguinte = newNode;
+
+}
+
+nodoDuplo insereInicio(nodoDuplo*& head, Carro& carro_a_receber) {
+	nodoDuplo* newNode = new nodoDuplo;
+	newNode->carros_a_ser_reparados = carro_a_receber;
+	newNode->seguinte = head;
+	newNode->anterior = NULL;
+
+	if (head != NULL) {
+		head->anterior = newNode;
+	}
+	head = newNode;
+
+}
+void insereMeio(nodoDuplo*& anterior, Carro& carro_a_receber)
+{
+	/*1. check if the given prev_node is NULL */
+	if (anterior == NULL) {
+		cout << "the given previous node cannot be NULL";
+		return;
+	}
+
+	/* 2. allocate new node */
+	nodoDuplo* newNode = new nodoDuplo;
+
+	/* 3. put in the data */
+	newNode->carros_a_ser_reparados = carro_a_receber;
+
+	/* 4. Make next of new node as next of prev_node */
+	newNode->seguinte = anterior->seguinte;
+
+	/* 5. Make the next of prev_node as new_node */
+	anterior->seguinte = newNode;
+
+	/* 6. Make prev_node as previous of new_node */
+	newNode->anterior = anterior;
+
+	/* 7. Change previous of new_node's next node */
+	if (newNode->seguinte != NULL)
+		newNode->seguinte->anterior = newNode;
+}
